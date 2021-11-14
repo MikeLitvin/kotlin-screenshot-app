d46c1e78d91377e107cd33454687425da332d644
Mike Litvin <mrlitvinm@gmail.com>
2021-11-14 18:27:42 +1000

    added shortcuts / changed  class name

--- src/main/kotlin/com/example/screenmaker/MainApplication.kt
+++ src/main/kotlin/com/example/screenmaker/MainApplication.kt
@@ -1,20 +1,35 @@
 package com.example.screenmaker
 
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyCodeCombination
+import javafx.scene.input.KeyCombination
 import javafx.stage.Stage
 
-class HelloApplication : Application() {
+class MainApplication : Application() {
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("main.fxml"))
+        val fxmlLoader = FXMLLoader(MainApplication::class.java.getResource("main.fxml"))
         val scene = Scene(fxmlLoader.load())
         stage.title = "Screenshot App"
         stage.scene = scene
         stage.show()
+        val exitShortcut = KeyCodeCombination(KeyCode.Q,
+            KeyCombination.CONTROL_DOWN)
+        val saveShortcut = KeyCodeCombination(KeyCode.S,
+            KeyCombination.CONTROL_DOWN)
+        val fastSaveShortcut = KeyCodeCombination(KeyCode.S,
+            KeyCombination.CONTROL_DOWN,
+            KeyCombination.SHIFT_DOWN)
+
+//        stage.scene.accelerators[exitShortcut] = Runnable {
+//            Platform.exit()
+//        }
     }
 }
 
 fun main() {
-    Application.launch(HelloApplication::class.java)
+    Application.launch(MainApplication::class.java)
 }
